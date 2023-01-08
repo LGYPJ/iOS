5a669b640df6d8b2824104bbea6b1e1a777a526f
hsw1920 <hsw1920@gmail.com>
2023-01-08 17:38:26 +0900

    Setting: No storyboard

---
+++ Garamgaebi/Garamgaebi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Garamgaebi
+//
+//  Created by 홍승완 on 2023/01/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .yellow
+    }
+
+
+}
+
