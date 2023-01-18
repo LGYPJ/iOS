a99c1ac1a7bde573ddf8d096a98870456c668f52
nahy-512 <hedghog1004@naver.com>
2023-01-18 17:13:15 +0900

    Feat: 내프로필 scrollview, 버튼 추가

--- Garamgaebi/Garamgaebi/View/Profile/ProfileVC.swift
+++ Garamgaebi/Garamgaebi/View/Profile/ProfileVC.swift
@@ -13,30 +13,26 @@ import Then
 class ProfileVC: UIViewController {
     
     // MARK: - Subviews
-    let scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.backgroundColor = .systemGray
-        
-        return scrollView
-    }()
+    let scrollView = UIScrollView()
+    
+    let contentView = UIView()
     
     let profileTitleLabel = UILabel().then {
         $0.text = "내 프로필"
-        $0.font = UIFont.NotoSansKR(type: .Bold, size: 25)
+        $0.font = UIFont.NotoSansKR(type: .Bold, size: 24)
     }
     
     let serviceBtn = UIButton().then {
-        $0.setImage(UIImage(systemName: "beats.headphones"), for: .normal)
+        $0.setImage(UIImage(named: "HeadsetMic"), for: .normal)
         $0.tintColor = .black
     }
     
-    let exitBtn = UIButton().then {
-        $0.setImage(UIImage(systemName: "rectangle.portrait.and.arrow.right"), for: .normal)
-        $0.tintColor = .black
+    let separateLine = UIView().then {
+        $0.backgroundColor = .mainGray
     }
     
     let profileImageView = UIImageView().then {
-        $0.layer.cornerRadius = 40
+        $0.layer.cornerRadius = 50
         $0.backgroundColor = .mainGray
     }
     
@@ -65,6 +61,18 @@ class ProfileVC: UIViewController {
         
         return stackView
     }()
+    
+    let introduceTextField = UITextView().then {
+        $0.layer.borderWidth = 0.8
+        $0.layer.borderColor = UIColor.systemGray5.cgColor // UIColor.lightGray.withAlphaComponent(0.7).cgColor
+        $0.layer.cornerRadius = 8
+        $0.textContainerInset = UIEdgeInsets(top: 10.0, left: 10.0, bottom: 16.0, right: 12.0)
+        $0.font = UIFont.NotoSansKR(type: .Regular, size: 14) // .systemFont(ofSize: 18)
+        $0.textColor = .black
+//        $0.isEditable = false
+        $0.isUserInteractionEnabled = false
+//        $0.numberOfLines = 0
+    }
     
     let profileEditBtn = UIButton().then {
         $0.setTitle("내 프로필 편집하기", for: .normal)
@@ -101,7 +109,6 @@ class ProfileVC: UIViewController {
     let addEduBtn = UIButton().then {
         let button = UIButton()
         $0.setTitle("교육 추가", for: .normal)
-//        self().setBtnLayout(button: button)
         $0.titleLabel?.font = UIFont.NotoSansKR(type: .Regular, size: 16)
         $0.setImage(UIImage(systemName: "plus"), for: .normal)
         $0.imageEdgeInsets = .init(top: 0, left: 0, bottom: 0, right: 15)
@@ -131,8 +138,6 @@ class ProfileVC: UIViewController {
         super.viewWillDisappear(animated)
         
         navigationController?.navigationBar.isHidden = false
-        // tabBarController?.tabBar.isHidden = true
-        
     }
     
     
@@ -140,98 +145,118 @@ class ProfileVC: UIViewController {
     func configureLayouts() {
         
         // addSubview
-        //view.addSubview(scrollView)
-        [profileTitleLabel, serviceBtn, exitBtn, profileImageView, profileStackView, profileEditBtn]
-            .forEach {view.addSubview($0)}
+        [profileTitleLabel, serviceBtn, separateLine]
+            .forEach { view.addSubview($0)}
         
-        [addSnsBtn, addCareerBtn, addEduBtn].forEach {view.addSubview($0)}
+        view.addSubview(scrollView)
+        
+        scrollView.addSubview(contentView)
+        
+        [profileImageView, profileStackView, introduceTextField, profileEditBtn]
+            .forEach {scrollView.addSubview($0)}
+        
+        [addSnsBtn, addCareerBtn, addEduBtn].forEach {scrollView.addSubview($0)}
+        
         
         // layer
-        /*
-        scrollView.snp.makeConstraints {
-            $0.top.equalToSuperview()
-            $0.bottom.equalToSuperview()
-            $0.leading.equalToSuperview()
-            $0.trailing.equalToSuperview()
-        }
-         */
-        
+        // title bar
         profileTitleLabel.snp.makeConstraints { /// 프로필 타이틀
 //            $0.top.equalTo(scrollView.snp.top)
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
             $0.leading.equalTo(21)
-            
+            $0.height.equalTo(70)
         }
         
         serviceBtn.snp.makeConstraints { /// 고객센터
-            $0.centerY.equalTo(exitBtn)
-            $0.trailing.equalTo(exitBtn.snp.leading).offset(-16)
-            $0.height.equalTo(24)
-            $0.width.equalTo(serviceBtn.snp.height)
-        }
-        /// 버튼 클릭
-        serviceBtn.addTarget(self,action: #selector(self.didTapServiceBtn(_:)), for: .touchUpInside)
-        
-        exitBtn.snp.makeConstraints { /// ??
             $0.centerY.equalTo(profileTitleLabel)
-            $0.trailing.equalTo(-23)
+            $0.trailing.equalTo(-21)
             $0.height.equalTo(24)
             $0.width.equalTo(serviceBtn.snp.height)
         }
         /// 버튼 클릭
+        serviceBtn.addTarget(self,action: #selector(self.serviceButtonDidTap(_:)), for: .touchUpInside)
         
+        separateLine.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(profileTitleLabel.snp.bottom)
+            $0.height.equalTo(1)
+        }
+        
+        
+        // scrollView
+        scrollView.snp.makeConstraints {
+//            $0.edges.equalToSuperview()
+            $0.top.equalTo(profileTitleLabel.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-10)
+        }
+        
+        // contentView
+        contentView.snp.makeConstraints {
+//            $0.edges.equalTo(scrollView)
+            $0.top.bottom.leading.trailing.equalTo(scrollView)
+            $0.width.equalTo(scrollView)
+        }
         
         profileImageView.snp.makeConstraints { /// 프로필 이미지
-            $0.width.equalTo(80)
+            $0.width.equalTo(100)
             $0.height.equalTo(profileImageView.snp.width)
-            $0.top.equalTo(profileTitleLabel.snp.bottom).offset(27)
-            $0.leading.equalTo(profileTitleLabel.snp.leading).offset(4)
+            $0.top.equalTo(contentView).offset(16)
+            //$0.top.equalTo(profileTitleLabel.snp.bottom).offset(27)
+            $0.leading.equalTo(16)
         }
         
         profileStackView.snp.makeConstraints { /// 이름, 소속, 링크 스택뷰
             $0.top.equalTo(profileImageView.snp.bottom).offset(16)
-            $0.leading.equalTo(profileImageView.snp.leading).offset(8)
+            $0.leading.equalTo(16)
+            $0.trailing.equalTo(-16)
+        }
+        
+        introduceTextField.snp.makeConstraints { /// 자기소개
+            $0.top.equalTo(profileStackView.snp.bottom).offset(16)
+            $0.leading.trailing.equalTo(profileStackView)
+            $0.height.equalTo(100)
         }
         
         profileEditBtn.snp.makeConstraints { /// 프로필 편집 버튼
-            $0.top.equalTo(profileStackView.snp.bottom).offset(19)
-            $0.leading.equalTo(22)
-            $0.trailing.equalTo(-22)
-            $0.height.equalTo(45)
+            $0.top.equalTo(introduceTextField.snp.bottom).offset(19)
+            $0.leading.trailing.equalTo(introduceTextField)
+            $0.height.equalTo(48)
         }
         /// 버튼 클릭
-        profileEditBtn.addTarget(self,action: #selector(self.didTapEditBtn(_:)), for: .touchUpInside)
+        profileEditBtn.addTarget(self,action: #selector(self.editButtonDidTap(_:)), for: .touchUpInside)
         
         addSnsBtn.snp.makeConstraints { /// SNS 추가 버튼
-            $0.top.equalTo(profileEditBtn.snp.bottom).offset(5)
+            $0.top.equalTo(profileEditBtn.snp.bottom).offset(100)
             $0.centerX.equalTo(view.safeAreaLayoutGuide)
             $0.height.equalTo(48)
             $0.width.equalTo(134)
         }
         /// 버튼 클릭
-        addSnsBtn.addTarget(self,action: #selector(self.didTapSnsBtn(_:)), for: .touchUpInside)
+        addSnsBtn.addTarget(self,action: #selector(self.snsButtonDidTap(_:)), for: .touchUpInside)
         
         addCareerBtn.snp.makeConstraints { /// 경력 추가 버튼
-            $0.top.equalTo(addSnsBtn.snp.bottom).offset(5)
+            $0.top.equalTo(addSnsBtn.snp.bottom).offset(100)
             $0.centerX.equalTo(view.safeAreaLayoutGuide)
             $0.height.equalTo(addSnsBtn.snp.height)
             $0.width.equalTo(addSnsBtn.snp.width)
         }
         /// 버튼 클릭
-        addCareerBtn.addTarget(self,action: #selector(self.didTapCareerBtn(_:)), for: .touchUpInside)
+        addCareerBtn.addTarget(self,action: #selector(self.careerButtonDidTap(_:)), for: .touchUpInside)
         
         addEduBtn.snp.makeConstraints { /// 교육 추가 버튼
-            $0.top.equalTo(addCareerBtn.snp.bottom).offset(5)
+            $0.top.equalTo(addCareerBtn.snp.bottom).offset(100)
             $0.centerX.equalTo(view.safeAreaLayoutGuide)
             $0.height.equalTo(addSnsBtn.snp.height)
             $0.width.equalTo(addSnsBtn.snp.width)
+            $0.bottom.equalTo(contentView)
         }
         /// 버튼 클릭
-        addEduBtn.addTarget(self,action: #selector(self.didTapEduBtn(_:)), for: .touchUpInside)
+        addEduBtn.addTarget(self,action: #selector(self.educationButtonDidTap(_:)), for: .touchUpInside)
     }
     
     
-    @objc private func didTapServiceBtn(_ sender : UIButton) {
+    @objc private func serviceButtonDidTap(_ sender : UIButton) {
         print("고객센터 버튼 클릭")
         
         // 화면 전환
@@ -239,7 +264,7 @@ class ProfileVC: UIViewController {
         navigationController?.pushViewController(nextVC, animated: true)
     }
     
-    @objc private func didTapEditBtn(_ sender : UIButton) {
+    @objc private func editButtonDidTap(_ sender : UIButton) {
         print("프로필 편집 버튼 클릭")
         
         // 화면 전환
@@ -247,7 +272,7 @@ class ProfileVC: UIViewController {
         navigationController?.pushViewController(nextVC, animated: true)
     }
     
-    @objc private func didTapSnsBtn(_ sender : UIButton) {
+    @objc private func snsButtonDidTap(_ sender : UIButton) {
         print("SNS 추가 버튼 클릭")
         
 //        // 화면 전환
@@ -255,7 +280,7 @@ class ProfileVC: UIViewController {
 //        navigationController?.pushViewController(nextVC, animated: true)
     }
     
-    @objc private func didTapCareerBtn(_ sender : UIButton) {
+    @objc private func careerButtonDidTap(_ sender : UIButton) {
         print("경력 추가 버튼 클릭")
         
         // 화면 전환
@@ -263,7 +288,7 @@ class ProfileVC: UIViewController {
         navigationController?.pushViewController(nextVC, animated: true)
     }
     
-    @objc private func didTapEduBtn(_ sender : UIButton) {
+    @objc private func educationButtonDidTap(_ sender : UIButton) {
         print("교육 추가 버튼 클릭")
         
         // 화면 전환
@@ -277,6 +302,7 @@ class ProfileVC: UIViewController {
         nameLabel.text = "코코아"
         orgLabel.text = "가천대학교 소프트웨어학과"
         emailLabel.text = "umc@gmail.com"
+        introduceTextField.text = "자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소개자기소."
     }
     
 }
