1630d97635788fa8655d6f4996a2a4d82e3603d6
HyunWoo Jeong <jrwedo@naver.com>
2023-01-20 18:29:04 +0900

    Setting: textfield, string extension 추가

--- Garamgaebi/Garamgaebi/View/ViewController/Register/SchoolEmailAuthVC.swift
+++ Garamgaebi/Garamgaebi/View/ViewController/Register/SchoolEmailAuthVC.swift
@@ -356,44 +356,7 @@ class SchoolEmailAuthVC: UIViewController {
     
 }
 
-extension UITextField {
-  func addLeftPadding() {
-    let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: self.frame.height))
-    self.leftView = paddingView
-    self.leftViewMode = ViewMode.always
-  }
-}
 
-extension UITextField {
-    
-    func setPlaceholderColor(_ placeholderColor: UIColor) {
-        attributedPlaceholder = NSAttributedString(
-            string: placeholder ?? "",
-            attributes: [
-                .foregroundColor: placeholderColor,
-                .font: font
-            ].compactMapValues { $0 }
-        )
-    }
-    
-}
 
-extension String {
-    // 숫자 6자일때 -> True
-    func isValidAuthNumber() -> Bool {
-        let authNumberRegEx = "[0-9]{6}"
-        let authNumberTest = NSPredicate.init(format: "SELF MATCHES %@", authNumberRegEx)
-        
-        return authNumberTest.evaluate(with: self)
-    }
-    
-    // id 정규표현식 5~13자
-    func isValidId() -> Bool {
-        let idRegEx = "[A-Za-z0-9]{5,13}"
-        let idTest = NSPredicate(format: "SELF MATCHES %@", idRegEx)
-        
-        return idTest.evaluate(with: self)
-    }
-}
 
 
