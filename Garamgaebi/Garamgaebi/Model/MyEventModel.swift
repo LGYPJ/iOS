81b8be2dc48a0e35636b08fa455bf122a67b30a3
hsw1920 <hsw1920@gmail.com>
2023-02-02 16:25:18 +0900

    Feat: 내 모임 ResponseModel 추가

---
+++ Garamgaebi/Garamgaebi/Model/MyEventModel.swift
@@ -0,0 +1,46 @@
+//
+//  MyEventModel.swift
+//  Garamgaebi
+//
+//  Created by 홍승완 on 2023/02/02.
+//
+
+import Foundation
+
+// MARK: 내 모임
+
+struct MyEventInfoReadyResponse: Decodable {
+    let isSuccess: Bool
+    let code: Int
+    let message: String
+    let result: [MyEventInfoReady]?
+}
+
+struct MyEventInfoReady: Decodable {
+    let programIdx: Int
+    let title: String
+    let date: String
+    let location: String
+    let type: String
+    let payment: String
+    let status: String
+    let isOpen: String
+}
+
+struct MyEventInfoCloseResponse: Decodable {
+    let isSuccess: Bool
+    let code: Int
+    let message: String
+    let result: [MyEventInfoReady]?
+}
+
+struct MyEventInfoClose: Decodable {
+    let programIdx: Int
+    let title: String
+    let date: String
+    let location: String
+    let type: String
+    let payment: String
+    let status: String
+    let isOpen: String
+}
