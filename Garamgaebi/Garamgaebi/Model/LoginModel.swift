cdfe66d77b2a70b2d8c2fb8db54bc3697575ed98
HyunWoo Jeong <jrwedo@naver.com>
2023-02-03 20:44:59 +0900

    Feat: Login API 연동 구현

---
+++ Garamgaebi/Garamgaebi/Model/LoginModel.swift
@@ -0,0 +1,22 @@
+//
+//  LoginModel.swift
+//  Garamgaebi
+//
+//  Created by 정현우 on 2023/02/03.
+//
+
+import Foundation
+
+struct LoginModelResponse: Codable {
+	let isSuccess: Bool
+	let code: Int
+	let message: String
+	let result: LoginModel
+}
+
+struct LoginModel: Codable {
+	let grantType: String
+	let accessToken: String
+	let refreshToken: String
+	let refreshTokenExpirationTime: Int
+}
---
+++ Garamgaebi/Garamgaebi/ViewModel/LoginViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewModel.swift
+//  Garamgaebi
+//
+//  Created by 정현우 on 2023/02/03.
+//
+
+import Alamofire
+
+class LoginViewModel {
+	
+	public static func postLogin(uniEmail: String, password: String, completion: @escaping ((LoginModel) -> Void)) {
+		let url = "https://garamgaebi.shop/member/login"
+		
+		let body: [String: Any] = [
+			"uniEmail": uniEmail,
+			"password": password
+		]
+		
+		AF.request(url, method: .post, parameters: body, encoding: JSONEncoding.default)
+			.validate()
+			.responseDecodable(of: LoginModelResponse.self) { response in
+				switch response.result {
+				case .success(let result):
+					if result.isSuccess {
+						completion(result.result)
+					} else {
+						print("실패(로그인): \(result.message)")
+					}
+				case .failure(let error):
+					print("실패(AF-로그인): \(error.localizedDescription)")
+				}
+			}
+		
+	}
+}
