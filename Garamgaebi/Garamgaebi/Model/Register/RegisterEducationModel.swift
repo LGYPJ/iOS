4cdb2c67bd50fec19575726929f8466dbdf54aa9
hsw1920 <hsw1920@gmail.com>
2023-02-04 18:40:14 +0900

    Feat: 가입 교육 responseModel 추가

--- Garamgaebi/Garamgaebi/Model/Register/RegisterEducationModel.swift
+++ Garamgaebi/Garamgaebi/Model/Register/RegisterEducationModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct RegisterEducationResponse: Codable {
+    let isSuccess: Bool
+    let code: Int
+    let message: String
+    let result: Bool
+    
+}
+struct RegisterEducationInfo: Codable {
+    let memberIdx: Int
+    let institution: String
+    let major: String
+    let isLearning: String
+    let startDate: String
+    let endDate: String
+}
