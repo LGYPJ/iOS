3acb9ba9757994d62e76314f6cf6a77c3a67bfb4
hsw1920 <hsw1920@gmail.com>
2023-02-11 14:33:36 +0900

    Design: 홈 Event 툴킷 이미지 변경, shadow 추가

---
+++ Garamgaebi/Garamgaebi/Extension/UIImage+.swift
@@ -0,0 +1,18 @@
+//
+//  UIImage+.swift
+//  Garamgaebi
+//
+//  Created by 홍승완 on 2023/02/11.
+//
+
+import UIKit
+
+extension UIImage {
+    func image(alpha: CGFloat) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        draw(at: .zero, blendMode: .normal, alpha: alpha)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+}
--- Garamgaebi/Garamgaebi/View/Cell/Home/HomeEventInfoTabelViewCell.swift
+++ Garamgaebi/Garamgaebi/View/Cell/Home/HomeEventInfoTabelViewCell.swift
@@ -338,8 +338,8 @@ class HomeEventInfoTableViewCell: UITableViewCell {
     
     @objc func presentHomeSeminarPopUpVC(_ touch: UITapGestureRecognizer) {
         NotificationCenter.default.post(name: Notification.Name("getScrollDirection"), object: nil)
-        popUpX = seminarPopUpButton.layer.frame.midX - 28
-        popUpY = seminarPopUpButton.layer.frame.midY + 80 - scrollDirection
+        popUpX = seminarPopUpButton.layer.frame.midX - 32
+        popUpY = seminarPopUpButton.layer.frame.midY + 90.1 - scrollDirection
         
         let vc = HomeSeminarPopUpVC(x: popUpX, y: popUpY)
         NotificationCenter.default.post(name: Notification.Name("pushEventPopUpView"), object: vc)
@@ -347,8 +347,8 @@ class HomeEventInfoTableViewCell: UITableViewCell {
     
     @objc func presentHomeNetworkingPopUpVC(_ touch: UITapGestureRecognizer) {
         NotificationCenter.default.post(name: Notification.Name("getScrollDirection"), object: nil)
-        popUpX = networkingPopUpButton.layer.frame.midX - 28
-        popUpY = networkingPopUpButton.layer.frame.midY + 80 - scrollDirection
+        popUpX = networkingPopUpButton.layer.frame.midX - 32
+        popUpY = networkingPopUpButton.layer.frame.midY + 88.4 - scrollDirection
         
         let vc = HomeNetworkingPopUpVC(x: popUpX, y: popUpY)
         NotificationCenter.default.post(name: Notification.Name("pushEventPopUpView"), object: vc)
