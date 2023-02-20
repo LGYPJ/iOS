a106120d075a57326d7094010719161fd902653f
hsw1920 <hsw1920@gmail.com>
2023-02-20 18:08:22 +0900

    Fix: 로그인시 responseModel을 completion하여 로그인 성공, 실패를 vc에서 알 수 있도록 변경

--- Garamgaebi/Garamgaebi/ViewModel/LoginViewModel.swift
+++ Garamgaebi/Garamgaebi/ViewModel/LoginViewModel.swift
@@ -9,7 +9,7 @@ import Alamofire
 
 class LoginViewModel {
 	
-	public static func postLogin(socialEmail: String, completion: @escaping ((LoginModel) -> Void)) {
+	public static func postLogin(socialEmail: String, completion: @escaping ((Result<LoginModelResponse, AFError>) -> Void)) {
 		let url = "https://garamgaebi.shop/member/login"
 		
 		let body: [String: Any] = [
@@ -22,10 +22,12 @@ class LoginViewModel {
 				switch response.result {
 				case .success(let result):
 					if result.isSuccess {
-                        guard let passData = result.result else {return}
-                        completion(passData)
+//                        guard let passData = result.result else {return}
+//                        completion(passData)
+                        completion(response.result)
 					} else {
 						print("실패(로그인): \(result.message)")
+                        completion(response.result)
 					}
 				case .failure(let error):
 					print("실패(AF-로그인): \(error.localizedDescription)")
