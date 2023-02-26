987d5cfe2d77f16d68a033050578569eb3354c4e
HyunWoo Jeong <jrwedo@naver.com>
2023-02-26 15:22:01 +0900

    Chore: 아이스 브레이킹 모델 별도 파일로 분리

--- Garamgaebi/Garamgaebi/ViewModel/IcebreakingViewModel.swift
+++ Garamgaebi/Garamgaebi/ViewModel/IcebreakingViewModel.swift
@@ -7,58 +7,6 @@
 
 import Alamofire
 
-// 유저 추가
-struct IceBreakingChangeUserModelResposne: Codable {
-	let isSuccess: Bool
-	let code: Int
-	let message: String
-	let result: IceBreakingChangeUserModel?
-}
-
-struct IceBreakingChangeUserModel: Codable {
-	let message: String
-	let currentImgIdx: Int
-	let currentMemberIdx: Int
-}
-
-// 현재 유저 조회
-struct IceBrakingCurrentUserModelResponse: Codable {
-	let isSuccess: Bool
-	let code: Int
-	let message: String
-	let result: [IceBrakingCurrentUserModel]?
-}
-
-struct IceBrakingCurrentUserModel: Codable {
-	let memberIdx: Int
-	let nickname: String
-	let profileUrl: String?
-}
-
-// 유저 삭제
-struct IceBreakingDeleteUserModelResponse: Codable {
-	let isSuccess: Bool
-	let code: Int
-	let message: String
-	let result: String?
-}
-
-// 이미지 불러오기
-struct IceBreakingImageModelResponse: Codable {
-	let isSuccess: Bool
-	let code: Int
-	let message: String
-	let result: [String]?
-}
-
-// 인덱스 PATCH
-struct IceBreakingPatchIndexModelResponse: Codable {
-	let isSuccess: Bool
-	let code: Int
-	let message: String
-	let result: String?
-}
-
 struct IcebreakingViewModel {
 	// 게임방 입장
 	public static func postGameUser(roomId: String, memberId: Int, completion: @escaping ((IceBreakingChangeUserModel) -> Void)) {
