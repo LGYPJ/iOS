68b563d878aa0542439a1186e0bd9445a669360c
hsw1920 <hsw1920@gmail.com>
2023-03-07 22:31:20 +0900

    Fix: 유효하지 않은 토큰의 경우 소셜로그인 화면으로 이동

--- Garamgaebi/Garamgaebi/View/ViewController/Splash/SplashVC.swift
+++ Garamgaebi/Garamgaebi/View/ViewController/Splash/SplashVC.swift
@@ -49,6 +49,12 @@ class SplashVC: UIViewController {
         vc.modalTransitionStyle = .crossDissolve
         present(vc, animated: true)
     }
+    private func showLogin() {
+        let vc = LoginVC()
+        vc.modalPresentationStyle = .fullScreen
+        vc.modalTransitionStyle = .crossDissolve
+        present(vc, animated: true)
+    }
     
     private func login() {
         autoLogin()
@@ -56,8 +62,7 @@ class SplashVC: UIViewController {
     
     public func autoLogin() {
         let refreshToken = UserDefaults.standard.string(forKey: "refreshToken") ?? ""
-        
-        LoginViewModel.postLoginAuto(refreshToken: "refreshToken", completion: { [weak self] result in
+        LoginViewModel.postLoginAuto(refreshToken: refreshToken, completion: { [weak self] result in
             switch result {
             case .success(let result):
                 if result.isSuccess {
@@ -66,10 +71,17 @@ class SplashVC: UIViewController {
                     UserDefaults.standard.set(result.result?.refreshToken, forKey: "refreshToken")
                     UserDefaults.standard.set(result.result?.memberIdx, forKey: "memberIdx")
                     self?.showHome()
-                } else {
-                    print(">>> Onboarding 화면으로 이동")
+                } else if result.code == 2006 {
+                    // 유효하지 않은 토큰의 경우 소셜로그인으로 이동
+                    print(">>> 소셜로그인 화면으로 이동 (유효하지 않은 토큰)")
+                    print("실패(자동로그인): \(result.message)")
+                    self?.showLogin()
+                }
+                else {
+                    print(">>> Onboarding 화면으로 이동 (서버오류, ex.최초로그인시에는 refreshToken이 존재하지 않음, 또는 올바르지 않은 토큰)")
                     print("실패(자동로그인): \(result.message)")
                     self?.showOnboarding()
+                    
                 }
             case .failure(let error):
                 // TODO: 아래와 같음
