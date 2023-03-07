31e098762eb474c1e810f713c4b0c75d4f46ebd0
hsw1920 <hsw1920@gmail.com>
2023-03-07 22:12:22 +0900

    Feat: 자동로그인 API 추가

--- Garamgaebi/Garamgaebi/ViewModel/LoginViewModel.swift
+++ Garamgaebi/Garamgaebi/ViewModel/LoginViewModel.swift
@@ -25,15 +25,41 @@ class LoginViewModel {
 					if result.isSuccess {
                         completion(response.result)
 					} else {
-						print("실패(로그인): \(result.message)")
+						print("실패(카카오 로그인): \(result.message)")
                         completion(response.result)
 					}
 				case .failure(let error):
                     completion(response.result)
-					print("실패(AF-로그인): \(error.localizedDescription)")
+					print("실패(AF-카카오 로그인): \(error.localizedDescription)")
 				}
 			}
 		
 	}
-
+    
+    public static func postLoginAuto(refreshToken: String, completion: @escaping ((Result<LoginModelResponse, AFError>) -> Void)) {
+        let url = "https://garamgaebi.shop/member/login/auto"
+        
+        let body: [String: Any] = [
+            "refreshToken": refreshToken,
+        ]
+        
+        AF.request(url, method: .post, parameters: body, encoding: JSONEncoding.default)
+            .validate()
+            .responseDecodable(of: LoginModelResponse.self) { response in
+                switch response.result {
+                case .success(let result):
+                    if result.isSuccess {
+                        completion(response.result)
+                    } else {
+                        print("실패(자동 로그인): \(result.message)")
+                        completion(response.result)
+                    }
+                case .failure(let error):
+                    completion(response.result)
+                    print("실패(AF-자동 로그인): \(error.localizedDescription)")
+                }
+            }
+        
+    }
+    
 }
