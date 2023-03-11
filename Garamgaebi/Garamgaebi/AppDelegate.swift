447e27dbc473f3f327e87da6709d90e3bed09420
hsw1920 <hsw1920@gmail.com>
2023-03-11 13:55:14 +0900

    Fix: Firebase 푸시알림 연동

--- Garamgaebi/Garamgaebi/AppDelegate.swift
+++ Garamgaebi/Garamgaebi/AppDelegate.swift
@@ -11,7 +11,9 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         // Native App Key 보안
@@ -22,53 +24,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Firebase 프로젝트에서 사용
         FirebaseApp.configure()
         Messaging.messaging().delegate = self
-        
         UNUserNotificationCenter.current().delegate = self
         let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { granted, _ in
-            if granted {
-                print("알림 등록이 완료되었습니다.")
-            }
-        }
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions,completionHandler: {_, _ in })
         application.registerForRemoteNotifications()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
-}
-
-extension AppDelegate: UNUserNotificationCenterDelegate {
+    
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Messaging.messaging().apnsToken = deviceToken
     }
-
-    // foreground 상에서 알림이 보이게끔 해준다.
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.banner, .sound, .badge])
-    }
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("\(#function)")
+    }
+}
 extension AppDelegate: MessagingDelegate {
-
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        // 여기서 이제 서버로 다시 fcm 토큰을 보내줘야 한다!
-        // 그러나 서버가 없기 때문에 이렇게 token을 출력하게 한다.
-        // 이 토큰은 뒤에서 Test할때 필요하다!
-        UserDefaults.standard.set(fcmToken!, forKey: "fcmToken")
-        print("FCM Token: \(fcmToken!)")
+        print("Firebase registration token: \(fcmToken)")
+        let dataDict:[String: String?] = ["token": fcmToken]
+        NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
     }
 }
