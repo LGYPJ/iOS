06713e822f320e46860b2ed1f223b534839560a9
hsw1920 <hsw1920@gmail.com>
2023-03-13 00:28:43 +0900

    Feat: 애플 로그인 API 추가

--- Garamgaebi/Garamgaebi/ViewModel/LoginViewModel.swift
+++ Garamgaebi/Garamgaebi/ViewModel/LoginViewModel.swift
@@ -18,11 +18,7 @@ class LoginViewModel {
             "accessToken": accessToken,
             "fcmToken": fcmToken
         ]
-        print(">>")
-        print(">>")
-        print(body)
-        print(">>")
-        print(">>")
+
         AF.request(url, method: .post, parameters: body, encoding: JSONEncoding.default)
             .validate()
             .responseDecodable(of: LoginModelResponse.self) { response in
@@ -37,6 +33,33 @@ class LoginViewModel {
                 case .failure(let error):
                     completion(response.result)
                     print("실패(AF-카카오 로그인): \(error.localizedDescription)")
+                }
+            }
+        
+    }
+    
+    public static func postLoginApple(idToken: String, fcmToken: String, completion: @escaping ((Result<LoginModelResponse, AFError>) -> Void)) {
+        let url = "https://garamgaebi.shop/member/login/kakao"
+
+        let body: [String: Any] = [
+            "idToken": idToken,
+            "fcmToken": fcmToken
+        ]
+
+        AF.request(url, method: .post, parameters: body, encoding: JSONEncoding.default)
+            .validate()
+            .responseDecodable(of: LoginModelResponse.self) { response in
+                switch response.result {
+                case .success(let result):
+                    if result.isSuccess {
+                        completion(response.result)
+                    } else {
+                        print("실패(애플 로그인): \(result.message)")
+                        completion(response.result)
+                    }
+                case .failure(let error):
+                    completion(response.result)
+                    print("실패(AF-애플 로그인): \(error.localizedDescription)")
                 }
             }
         
